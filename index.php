acf6526372095ea4533a5565a2561e9c69b98702
Patrik Felbinger <patrik.felbinger@web.de>
2022-04-04 22:24:13 +0200

    validates POST Input and displays invalid message

--- index.php
+++ index.php
@@ -8,8 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -21,66 +20,82 @@
 
                 <div class="form-group">
                     <label for="bikePicker">Wie viele eBikes brauchen Sie?</label>
-                    <input type="number" name="bikes" id="bikePicker" class="form-control"
-                        placeholder="Anzahl an eBikes..." aria-describedby="helpId">
+                    <input type="number" name="bikes" id="bikePicker" class="form-control" placeholder="Anzahl an eBikes..." aria-describedby="helpId">
                     <small id="helpId" class="text-muted">Geben Sie an wie viele eBikes Sie an diesem Tage
                         brauchen</small>
                 </div>
                 <div class="form-group">
                     <label for="datePicker">Wählen Sie ein Datum</label>
-                    <input type="date" name="date" id="datePicker" class="form-control" placeholder=""
-                        aria-describedby="helpId">
+                    <input type="date" name="date" id="datePicker" class="form-control" placeholder="" aria-describedby="helpId">
                 </div>
                 <input type="submit" class="btn btn-warning" value="Buchen">
             </form>
         </div>
-        
-            <?php   
-                include 'database.php';             
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                  $bikes = test_input($_POST["bikes"]);
-                  $date = test_input($_POST["date"]);
-                  $success = insertBooking($date, $bikes);
-                  switch($success){
-                      case BookingStatus::Success: 
-                        echo '<div class="jumbotron bg-success">'; 
+
+        <?php
+        include 'database.php';
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            //bikes und date auf Gültigkeit validieren
+            $bikes = test_input($_POST["bikes"]);
+            $date = test_input($_POST["date"]);
+            if (is_valid_input($bikes, $date)) {
+                $success = insertBooking($date, $bikes);
+                switch ($success) {
+                    case BookingStatus::Success:
+                        echo '<div class="jumbotron bg-success">';
                         echo "<h4 class='text-center font-weight-normal'>Sie haben " . $bikes . " eBikes für den " . $date . " gebucht!</h4>";
                         echo '</div>';
                         break;
                     case BookingStatus::Duplicate:
-                        echo '<div class="jumbotron bg-info">'; 
+                        echo '<div class="jumbotron bg-info">';
                         echo "<h4 class='text-center font-weight-normal'>Sie haben bereits eine Buchung für den " . $date . " vorgenommen.
-                        Passen Sie die Buchung unter <em>Meine Buchungen</em> an</h4>";
-                        echo '</div>'; 
+                            Passen Sie die Buchung unter <em>Meine Buchungen</em> an</h4>";
+                        echo '</div>';
                         break;
                     case BookingStatus::Error:
-                        echo '<div class="jumbotron bg-danger">'; 
+                        echo '<div class="jumbotron bg-danger">';
                         echo "<h4 class='text-center font-weight-normal'>Der Buchungsvorgang war leider nicht erfolgreich. Versuchen Sie
-                        es nochmal. Falls das Problem besteht, kontaktieren Sie den Kundenservice.</h4>";
-                        echo '</div>'; 
-                  }                                                 
+                            es nochmal. Falls das Problem besteht, kontaktieren Sie den Kundenservice.</h4>";
+                        echo '</div>';
                 }
-                
-                function test_input($data) {
-                  $data = trim($data);
-                  $data = stripslashes($data);
-                  $data = htmlspecialchars($data);
-                  return $data;
+            } else {
+                echo '<div class="jumbotron bg-info">
+                        <h4 class="text-center font-weight-normal">Ung&uuml;ltige Eingabe</h4>
+                        <p class="text-center font-weight-normal">Bitte geben Sie eine Ganzzahl an Fahrr&auml;dern an und wählen Sie ein zukünftiges Datum aus</p>
+                        </div>';
+            }
+        }
+
+        function test_input($data)
+        {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        function is_valid_input($bikes, $date)
+        {
+            try {
+                (int)$bikes;
+                $today = new DateTime();
+                $date_d = new DateTime($date);
+                if ($today > $date_d) {
+                    return false;
                 }
-            ?>
-        
+                return true;
+            } catch (Exception $e) {
+                return (false);
+            }
+        }
+        ?>
+
     </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script>
         console.log("works")
         //document.getElementById('datePicker').value = new Date().toDateInputValue();
