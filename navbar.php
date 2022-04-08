ea6691669d404fd3adcde05f160f7a955853dcb5
Patrik Felbinger <patrik.felbinger@web.de>
2022-04-08 17:12:51 +0200

    Adds navbar user name and logout button

--- navbar.php
+++ navbar.php
@@ -1,10 +1,12 @@
 
 <?php
- echo '
+echo '
  <div class="container-flex bg-dark p-2">
  <nav class="nav justify-content-center">
      <a class="nav-link text-white mr-5" href="index.php">eBikes buchen</a>
      <a class="nav-link text-white ml-5" href="my_requests.php">Meine Buchungen</a>
+     <a class="nav-link text-white ml-5" href="#">Hotel Sommerweide</a>
+     <a class="nav-link text-white ml-5" href="#">Logout</a>
  </nav>
 </div>
 ';
