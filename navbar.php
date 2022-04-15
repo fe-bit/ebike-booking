ffc088a0692bfac43beacf2e7a696983fe3ea206
Patrik Felbinger <patrik.felbinger@web.de>
2022-04-15 23:33:55 +0200

    Responsive Navbar

--- navbar.php
+++ navbar.php
@@ -1,13 +1,29 @@
-
 <?php
-echo '
- <div class="container-flex bg-dark p-2">
- <nav class="nav justify-content-center">
-     <a class="nav-link text-white mr-5" href="index.php">eBikes buchen</a>
-     <a class="nav-link text-white ml-5" href="my_requests.php">Meine Buchungen</a>
-     <a class="nav-link text-white ml-5" href="#">Hotel Sommerweide</a>
-     <a class="nav-link text-white ml-5" href="#">Logout</a>
- </nav>
+echo '<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+<!-- Brand -->
+<a class="navbar-brand" href="index.php">eBike Buchungsplattform</a>
+
+<!-- Toggler/collapsibe Button -->
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<!-- Navbar links -->
+<div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.php">eBikes buchen</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="my_requests.php">Meine Buchungen</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Mein Account</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Logout</a>
+        </li>
+    </ul>
 </div>
-';
+</nav>';
 ?>
