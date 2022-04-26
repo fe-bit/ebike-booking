5106cc126adcd6f8be2ba5e9956ed6b04b03cdb7
Patrik Felbinger <patrik.felbinger@web.de>
2022-04-26 18:54:02 +0200

    Validates integer bikes

--- database.php
+++ database.php
@@ -3,24 +3,27 @@
 
 
 
-function SetUpDatabase(){
+function SetUpDatabase()
+{
     $dbusername = "root";
     $dbpassword = "";
     $dbname = "ebike_bookings";
     $pdo = new PDO("mysql:host=localhost", $dbusername, $dbpassword);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $dbname = "`".str_replace("`","``",$dbname)."`";
+    $dbname = "`" . str_replace("`", "``", $dbname) . "`";
     $pdo->query("CREATE DATABASE IF NOT EXISTS $dbname");
     $pdo->query("use $dbname");
 }
 
-function GetDbConnection(){
+function GetDbConnection()
+{
     return new PDO('mysql:host=localhost;dbname=ebike_bookings;charset=utf8', 'root', '');
 }
 
-function setUpTable(){
-    try{
+function setUpTable()
+{
+    try {
         $sql = "CREATE TABLE IF NOT EXISTS `booking` (
             `id` INT AUTO_INCREMENT NOT NULL,
             `bikes` INT NOT NULL,
@@ -29,7 +32,7 @@ function setUpTable(){
           );";
         $pdo = GetDbConnection();
         $pdo->query($sql);
-    } catch (Exception $e){
+    } catch (Exception $e) {
         echo $e;
     }
 }
@@ -49,7 +52,7 @@ enum BookingStatus
 
 function getAllBookings($year, $month)
 {
-    $pdo = GetDbConnection();//new PDO('mysql:host=localhost;dbname=ebike_bookings;charset=utf8', 'bookingManager', 'addjsdfe093');
+    $pdo = GetDbConnection(); //new PDO('mysql:host=localhost;dbname=ebike_bookings;charset=utf8', 'bookingManager', 'addjsdfe093');
     $sql = "SELECT date_of_booking, bikes FROM booking WHERE YEAR(date_of_booking)='" . $year . "' AND MONTH(date_of_booking)='" . $month . "' ORDER BY date_of_booking ASC;";
     return $pdo->query($sql);
 }
@@ -65,6 +68,9 @@ function is_valid_input($bikes, $date)
 {
     try {
         $bikes_number = (int)$bikes;
+        if ($bikes != $bikes_number) {
+            return false; //bikes is not an integer
+        }
         if ($bikes_number <= 0) {
             return false;
         }
@@ -85,7 +91,7 @@ function insertBooking($date, $bikes)
         if (!Is_valid_input($bikes, $date)) {
             return BookingStatus::InvalidDateOrNumber;
         }
-        $pdo = GetDbConnection();//new PDO('mysql:host=localhost;dbname=ebike_bookings;charset=utf8', 'bookingManager', 'addjsdfe093');
+        $pdo = GetDbConnection(); //new PDO('mysql:host=localhost;dbname=ebike_bookings;charset=utf8', 'bookingManager', 'addjsdfe093');
         if (date_is_already_in_booking($pdo, $date)) {
             return BookingStatus::Duplicate;
         }
